3c1b0d995fb5317f4209a83910ef4d38983f7fa0
Garima <2412garima@gmail.com>
2025-09-05 21:10:04 +0530

    @Component and @Autowired usage

--- src/main/java/com/example/firstApp/Developer.java
+++ src/main/java/com/example/firstApp/Developer.java
@@ -1,12 +1,17 @@
 package com.example.firstApp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component //tells Spring that this class is a Spring managed bean
 public class Developer {
 
+    @Autowired  // tells Spring to give object of Laptop
+    private Laptop lap;
+
     public void build()
     {
+        lap.compile();
         System.out.println("Building a java project");
     }
 }
---
+++ src/main/java/com/example/firstApp/Laptop.java
@@ -0,0 +1,12 @@
+package com.example.firstApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+    public void compile()
+    {
+        System.out.println("Few errors found");
+    }
+}
