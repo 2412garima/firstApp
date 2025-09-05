43555d6b985560456e1ba47e152d61286856ade1
Garima <2412garima@gmail.com>
2025-09-05 21:26:03 +0530

    Constructor injection

--- src/main/java/com/example/firstApp/Developer.java
+++ src/main/java/com/example/firstApp/Developer.java
@@ -6,8 +6,20 @@ import org.springframework.stereotype.Component;
 @Component //tells Spring that this class is a Spring managed bean
 public class Developer {
 
-    @Autowired  // tells Spring to give object of Laptop
+//    @Autowired  // tells Spring to give object of Laptop, it is Field injection
     private Laptop lap;
+
+    //Constructor Injection, @Autowired comes by default
+    public Developer(Laptop lap) {
+        this.lap = lap;
+    }
+//
+//    //Setter injection, need to add @Autowired
+//    @Autowired
+//    public void setLap(Laptop lap) {
+//        this.lap = lap;
+//    }
+
 
     public void build()
     {
